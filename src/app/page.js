ad5a2d838c01ccf4e97967b660621f9aeee5f359
Jeff Wolff <jeffwolff@Jeffs-MacBook-Air.local>
2023-05-14 14:16:51 -0700

    init

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,52 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+const jsdom = require('jsdom');
+const { JSDOM } = jsdom;
+const { Readability } = require('@mozilla/readability');
+
+async function crawlArticle(url) {
+  // Create a JSDOM instance and fetch the webpage content
+  const dom = await JSDOM.fromURL(url);
+  const doc = dom.window.document;
+
+  // Create a Readability instance and parse the document
+  const reader = new Readability(doc);
+  const article = reader.parse();
+
+  // Extract the article title and content
+  const articleTitle = article.title;
+  const articleContent = article.textContent.replace(/\n/g, '').replace(/\s\s+/g, ' ');
+
+  return { title: articleTitle, content: articleContent };
+}
+
+
+async function processArticles(urls) {
+  for (const url of urls) {
+    try {
+      const result = await crawlArticle(url);
+      console.log('URL:', url);
+      console.log('Title:', result.title);
+      console.log('Content:', result.content);
+      console.log('------------------------------------------------------');
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+}
 
 export default function Home() {
+  const articleUrls = [
+    'https://abcnews.go.com/US/7-injured-shooting-yuma-arizona-gathering-police/story?id=99313241',
+    'https://www.foxnews.com/us/arizona-border-city-sees-7-people-shot-suspect-large-report',
+    'https://www.cnn.com/2023/05/14/us/2-dead-5-injured-yuma-arizona-shooting/index.html'
+  ];
+
+  processArticles(articleUrls);
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <h1>Extract News Article Body From Url</h1>
     </main>
-  )
+  );
 }
